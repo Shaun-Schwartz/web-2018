61eb95e693b939ce5c2d22d0ad60dae315ab974b
Shaun Schwartz <me@shaunschwartz.com>
2019-01-06 16:14:09 -0800

    fix compile errors

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Cookies from './components/cookies'
-import DownloadApp from './components/downloadApp'
 import Enter from './components/enter'
-import Location from './components/location'
-import SetHomepage from './components/setHomepage'
 import Subscribe from './components/subscribe'
 
 class App extends Component {
